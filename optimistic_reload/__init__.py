2c125ea32e2bc040e13aaac18433c47f74f24b3e
Dan Davison <dandavison7@gmail.com>
2019-04-13 15:54:08 -0700

    Build import dependency graph at import time
    https://stackoverflow.com/a/1811165/583763

--- optimistic_reload/__init__.py
+++ optimistic_reload/__init__.py
@@ -1,10 +1,29 @@
 import builtins
+import importlib
+import inspect
+import sys
+from collections import defaultdict
+from pprint import pprint
 
 
-__all__ = [__import__]
+__original_import__ = builtins.__import__
+_dependency_graph = defaultdict(set)
 
-__builtins__import__ = builtins.__import__
+
+def import_and_build_dependency_graph(name, *args, **kwargs):
+    child_module = __original_import__(name, *args, **kwargs)
+    if hasattr(child_module, '__file__'):
+        child_module_name = child_module.__name__
+        _dependency_graph[child_module.__file__].add(child_module_name)
+        caller = inspect.currentframe().f_back
+        parent_module_name = caller.f_globals.get('__name__')
+        _dependency_graph[child_module.__file__].add(parent_module_name)
+    return child_module
 
 
-def __import__(name, *args, **kwargs):
-    return __builtins__import__(name, *args, **kwargs)
+def reload(module_name):
+    module = sys.modules[module_name]
+    path = module.__file__
+    module_names_to_reload = {module_name} | _dependency_graph.get(path, set())
+    for module_name in module_names_to_reload:
+        importlib.reload(sys.modules[module_name])
