5dbb67a9ecb5a954366ab6c8f928ae34086e669e
Dan Davison <dandavison7@gmail.com>
2019-04-24 18:58:14 +0100

    Test optimistic reload

--- tests/test_optimistic_reload.py
+++ tests/test_optimistic_reload.py
@@ -1,6 +1,8 @@
 import sys
+from unittest.mock import patch
 
-from optimistic_reload.importlib import reload
+from optimistic_reload import import_and_build_dependency_graph
+from optimistic_reload import reload
 from .utils import Package
 
 
@@ -8,12 +10,13 @@ def test_single_module(tmp_path_factory):
     files = {
         'a.py': 'x = 1',
     }
-    with Package(tmp_path_factory, files) as package:
-        import a
-        assert a.x == 1
-        package.write({'a.py': 'x = 2'})
-        reload('a')
-        assert a.x == 2
+    with patch('builtins.__import__', import_and_build_dependency_graph):
+        with Package(tmp_path_factory, files) as package:
+            import a
+            assert a.x == 1
+            package.write({'a.py': 'x = 2'})
+            reload('a')
+            assert a.x == 2
 
 
 def test_import_module(tmp_path_factory):
@@ -21,12 +24,13 @@ def test_import_module(tmp_path_factory):
         'a.py': 'x = 1',
         'b.py': 'import a',
     }
-    with Package(tmp_path_factory, files) as package:
-        import b
-        assert b.a.x == 1
-        package.write({'a.py': 'x = 2'})
-        reload('a')
-        assert b.a.x == 2
+    with patch('builtins.__import__', import_and_build_dependency_graph):
+        with Package(tmp_path_factory, files) as package:
+            import b
+            assert b.a.x == 1
+            package.write({'a.py': 'x = 2'})
+            reload('a')
+            assert b.a.x == 2
 
 
 def test_import_object_from_module(tmp_path_factory):
@@ -34,9 +38,12 @@ def test_import_object_from_module(tmp_path_factory):
         'a.py': 'x = 1',
         'b.py': 'from a import x',
     }
-    with Package(tmp_path_factory, files) as package:
-        import b
-        assert b.x == 1
-        package.write({'a.py': 'x = 2'})
-        reload('a')
-        assert b.x == 2
+    with patch('builtins.__import__', import_and_build_dependency_graph):
+        with Package(tmp_path_factory, files) as package:
+            import b
+            assert b.x == 1
+            package.write({'a.py': 'x = 2'})
+            reload('a')
+            assert b.x == 2
+
+
