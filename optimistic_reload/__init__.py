7abbe865ee78a90ae1a2b91511e6fa444b602181
Dan Davison <dandavison7@gmail.com>
2019-05-01 09:13:25 +0100

    Error logging

--- optimistic_reload/__init__.py
+++ optimistic_reload/__init__.py
@@ -7,6 +7,11 @@ import networkx as nx
 from networkx.algorithms import ancestors
 from networkx.algorithms import topological_sort
 
+from .utils import blue
+from .utils import green
+from .utils import red
+
+
 __original_import__ = builtins.__import__
 _dependency_graph = nx.DiGraph()
 
@@ -27,5 +32,21 @@ def _ancestors_in_topological_sort_order(module_name):
 
 
 def reload(module_name):
-    for ancestral_module_name in _ancestors_in_topological_sort_order(module_name):
-        importlib.reload(sys.modules[ancestral_module_name])
+    if module_name not in _dependency_graph:
+        print(red(f'optimistic-reload: error: not in graph: {module_name}'))
+        return
+    else:
+        ancestral_module_names = _ancestors_in_topological_sort_order(module_name)
+
+    print(blue(f'optimistic-reload: reloading: {ancestral_module_names}'))
+
+    for ancestral_module_name in ancestral_module_names:
+        try:
+            importlib.reload(sys.modules[ancestral_module_name])
+        except Exception as ex:
+            print(red((f'optimistic-reload: '
+                       f'error while attempting reload({ancestral_module_name}): '
+                       f'{ex.__class__.__name__}({ex})')))
+
+    print(green(f'optimistic-reload: reloaded ancestors of {module_name}'))
+    print('\n')
