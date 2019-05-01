748e28571a9856a62bed972f9b6f8d9c425ad4e4
Dan Davison <dandavison7@gmail.com>
2019-05-01 15:20:44 +0100

    Insert root directory into test packages
    This is needed because we ignore imports inside function bodies, but the test suite relies on such
    run time imports.

--- tests/test_optimistic_reload.py
+++ tests/test_optimistic_reload.py
@@ -9,72 +9,72 @@ from .utils import Package
 
 def test_single_module(tmp_path_factory):
     files = {
-        'a.py': 'x = 1',
+        'a/a.py': 'x = 1',
     }
     with patch('builtins.__import__', import_and_build_dependency_graph):
         with Package(tmp_path_factory, files) as package:
-            import a
-            assert a.x == 1
-            package.write({'a.py': 'x = 2'})
-            reload('a')
-            assert a.x == 2
+            import a.a
+            assert a.a.x == 1
+            package.write({'a/a.py': 'x = 2'})
+            reload('a.a')
+            assert a.a.x == 2
 
 
 def test_import_module(tmp_path_factory):
     files = {
-        'a.py': 'x = 1',
-        'b.py': 'import a',
+        'a/a.py': 'x = 1',
+        'a/b.py': 'import a.a',
     }
     with patch('builtins.__import__', import_and_build_dependency_graph):
         with Package(tmp_path_factory, files) as package:
-            import b
-            assert b.a.x == 1
-            package.write({'a.py': 'x = 2'})
-            reload('a')
-            assert b.a.x == 2
+            import a.b
+            assert a.b.a.x == 1
+            package.write({'a/a.py': 'x = 2'})
+            reload('a.a')
+            assert a.b.a.x == 2
 
 
 def test_import_object_from_module(tmp_path_factory):
     files = {
-        'a.py': 'x = 1',
-        'b.py': 'from a import x',
+        'a/a.py': 'x = 1',
+        'a/b.py': 'from a.a import x',
     }
     with patch('builtins.__import__', import_and_build_dependency_graph):
         with Package(tmp_path_factory, files) as package:
-            import b
-            assert b.x == 1
-            package.write({'a.py': 'x = 2'})
-            reload('a')
-            assert b.x == 2
+            import a.b
+            assert a.b.x == 1
+            package.write({'a/a.py': 'x = 2'})
+            reload('a.a')
+            assert a.b.x == 2
 
 
 def test_import_object_from_deeply_nested_module(tmp_path_factory):
     files = {
-        'a.py': 'x = 1',
-        'b.py': 'from a import x',
-        'c.py': 'from b import x',
-        'd.py': 'from c import x',
+        'a/a.py': 'x = 1',
+        'a/b.py': 'from a.a import x',
+        'a/c.py': 'from a.b import x',
+        'a/d.py': 'from a.c import x',
     }
     with patch('builtins.__import__', import_and_build_dependency_graph):
         with Package(tmp_path_factory, files) as package:
-            import d
-            assert d.x == 1
-            package.write({'a.py': 'x = 2'})
-            reload('a')
-            assert d.x == 2
+            import a.d
+            assert a.d.x == 1
+            package.write({'a/a.py': 'x = 2'})
+            reload('a.a')
+            assert a.d.x == 2
 
 
 def test_run_time_imports_do_not_add_edges_to_graph(tmp_path_factory):
     files = {
-        'a.py': 'x = 1',
-        'b.py': (
+        'a/a.py': 'x = 1',
+        'a/b.py': (
             '''
             def f():
-                import a
+                import a.a
             '''),
     }
     with patch('builtins.__import__', import_and_build_dependency_graph):
         with Package(tmp_path_factory, files) as package:
-            import b
-            b.f()
-            assert not _dependency_graph.has_edge('b', 'a')
+            import a.b
+            a.b.f()
+            assert not _dependency_graph.has_edge('a.b', 'a.a')
