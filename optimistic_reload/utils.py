27a3c21898e528c0843805adf850ec5f011d1ac8
Dan Davison <dandavison7@gmail.com>
2019-05-01 09:11:51 +0100

    Color utilities

---
+++ optimistic_reload/utils.py
@@ -0,0 +1,6 @@
+from clint.textui import colored
+
+
+red = lambda s: colored.red(s, bold=True)
+green = lambda s: colored.green(s, bold=True)
+blue = lambda s: colored.blue(s, bold=True)
--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     include_package_data=True,
     zip_safe=True,
     install_requires=[
+        'clint',
         'networkx',
     ],
     tests_requires=[
