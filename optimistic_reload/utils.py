9fc1973145ca184496369a23dc714c71615be891
Dan Davison <dandavison7@gmail.com>
2019-06-09 20:46:48 +0200

    Logging utilities

--- optimistic_reload/utils.py
+++ optimistic_reload/utils.py
@@ -1,3 +1,4 @@
+import subprocess
 import sys
 import traceback
 
@@ -9,6 +10,26 @@ green = lambda s: colored.green(s, bold=True)
 blue = lambda s: colored.blue(s, bold=True)
 
 
+LOGFILE = open('/tmp/optimistic-reload.log', 'a')
+
+
+def log(msg, **kwargs):
+    print(msg, file=LOGFILE, flush=True)
+    if kwargs.get('notify', False):
+        notify(msg)
+
+
 def log_error(msg):
-    print('\n'.join(traceback.format_stack()), file=sys.stderr)
-    print(red(msg), file=sys.stderr)
+    print('\n'.join(traceback.format_stack()), file=LOGFILE, flush=True)
+    print(red(msg), file=LOGFILE, flush=True)
+    notify(msg)
+
+
+def notify(msg):
+    # TODO: Make this user-configurable
+    subprocess.call([
+        'terminal-notifier',
+        '-title', 'optimistic-reload',
+        '-message', msg,
+    ])
+
